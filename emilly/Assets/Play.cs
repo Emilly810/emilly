05a23207033107e35bbb1f9e7813f0f4b62870a2
Emilly810 <eduarda.emilly@escolar.ifrn.edu.br>
2025-05-05 12:06:28 -0300

    hoje eu escrevi varias coisas

---
+++ emilly/Assets/Play.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class player: monobehaviour
+
+private int numero:
+{public float velocidade * 40:
+    void Start()
+    {
+        numero= 0:
+    }
+
+    void Update()
+    {
+        //Debug/Log(numero);
+       //numero = numero + 1;1f (Input.getkey(KeyCode.A))
+    1f(Input.GetKey(KeyCode.A))
+    GameObject.transfora.position * +new vercto3(velocidade 0, 0);
+    
+    1f (Input.GetKey(KeyCode.D))
+    
+    
